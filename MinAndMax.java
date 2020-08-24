41558b35e6c69a70a09acf878af6e02850af2878
ludgate-dt <70148426+ludgate-dt@users.noreply.github.com>
2020-08-24 19:09:37 +0530

    Add files via upload

---
+++ MinAndMax.java
@@ -0,0 +1,24 @@
+package com.studies.java8.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class MinAndMax {
+
+	public static void main(String[] args) {
+		
+			List<Integer> intList = Arrays.asList(3, 4, 6, 7, 8 , 1, 2);
+			Integer minValue = intList.stream()
+													  .min(Integer::compareTo).get();
+			
+			System.out.println(minValue);
+			
+			Optional<Integer> maxValue = intList.stream()
+					  .max((a, b) -> a.compareTo(b));
+			
+			System.out.println(maxValue);
+
+	}
+
+}
